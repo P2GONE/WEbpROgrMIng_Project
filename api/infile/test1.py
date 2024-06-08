51e963b79d680e29ec657101f5b3b716d18f5202
JaeHun Gou <106510018+vlrhsgody@users.noreply.github.com>
2024-06-08 20:10:36 +0900

    알고리즘 오류 수정
    이제 될듯 ㅇㅇ 완성함

--- api/infile/test1.py
+++ api/infile/test1.py
@@ -26,7 +26,7 @@ async def create_task(db: Session = Depends(get_db)):
         url2='http://apis.data.go.kr/1160100/service/GetFinaStatInfoService_V2/getSummFinaStat_V2'
         #data = get_data('http://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo',p,1,'itmsNm')
         data2 = get_data(url2,p,1,'crno')
-        data = get_data2('http://apis.data.go.kr/1160100/service/GetCorpBasicInfoService_V2/getCorpOutline_V2?crno={data2}',p,1,'corpNm')
+        data = get_data('http://apis.data.go.kr/1160100/service/GetCorpBasicInfoService_V2/getCorpOutline_V2?crno={data2}',p,1,'corpNm')
         data3 = get_data(url2,p,1,'bizYear') # 사업연도
         data4 = get_data(url2,p,1,'enpSaleAmt') #기업매출금액
         data5 = get_data(url2,p,1,'enpBzopPft') #기업영업이익
@@ -37,13 +37,20 @@ async def create_task(db: Session = Depends(get_db)):
         data10 = get_data(url2,p,1,'enpTcptAmt') # 기업총자본금액
         data11 = get_data(url2,p,1,'enpCptlAmt') # 기업자본 금액
         data12 = get_data(url2,p,1,'fnclDebtRto') # 재무제포부채비율
-        task_body = task_schema.TaskCreate(title=data,crno=data2,year=data3,enpSaleAmt=data4,enpBzopPft=data5,iclsPalClcAmt=data6,enpCrtmNpf=data7,enpTastAmt=data8,enpTdbtAmt=data9,enpTcptAmt=data10,enpCptlAmt=data11,fnclDebtRto=data12)
+        task_body = task_schema.TaskCreate(
+            title=data,
+            crno=data2,
+            year=data3,
+            enpSaleAmt=data4,
+            enpBzopPft=data5,
+            iclsPalClcAmt=data6,
+            enpCrtmNpf=data7,
+            enpTastAmt=data8,
+            enpTdbtAmt=data9,
+            enpTcptAmt=data10,
+            enpCptlAmt=data11,
+            fnclDebtRto=data12
+        )
         created_task = task_crud.create_task(db, task_body)
         created_tasks.append(created_task)
     return created_tasks
-## 여기서 회사이름은 crno랑 관계가 없으므로 나중에 매핑을 해야함 부터의 값이랑 전혀 관계가 없음 따라서 할거면 
-@router.get("/tasks",response_model=list[task_schema.Task])
-async def list_task(db:Session = Depends(get_db)):
-    tasks = task_crud.get_tasks_with_done(db)
-    fourth_task = tasks[3]  # 0-based index, so the fourth element is at index 3
-    return {f"Fourth task: ID={fourth_task[0]}, Title={fourth_task[1]}"}
